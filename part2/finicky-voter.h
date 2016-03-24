2c335e397cfbf9480341a4ff5b60d016d2d820be
Adam Geipel (UWL) <geipel.adam@uwlax.edu>
2016-03-24 15:23:43 -0500

    got rid of unnecessary semophores

--- part2/finicky-voter.h
+++ part2/finicky-voter.h
@@ -14,30 +14,6 @@
 /*****************************
  * Defines
  *****************************/
-int rep_inline = 0; //num of republicans waiting in line for a booth
-int dem_inline = 0; //num of democrats waiting in line for a booth
-int num_republicans = 0;
-int num_democrats = 0;
-int num_independents = 0;
-int num_booths = 0;
-int total_voting = 0; //total people voting today (set in main)
-int total_finished = 0; //total num people finished voting and left polling station
-int total_entered = 0; //toal num entered the polling station
-int *buffer = NULL;
-int total_waiting = 0; //count for the initial print waiting for station to open
-int in = 0;
-
-pthread_t *rep_threads;
-pthread_t *dem_threads;
-pthread_t *ind_threads;
-semaphore_t protect_count, mutex_lineup, barrier, printing_mutex, can_vote;
-semaphore_t waiting_rep, waiting_dem, waiting_ind, rep_barrier, dem_barrier, free_booths;
-//add these to .c
-semaphore_t reps_wait_on_dems, dem_wait_on_reps;
-//protects the "in" count
-semaphore_t mutex_in;
-//for independents waiting to jump in line
-semaphore_t inds_waiting_for_line;
 
 /*****************************
  * Structures
@@ -47,6 +23,37 @@ semaphore_t inds_waiting_for_line;
 /*****************************
  * Global Variables
  *****************************/
+//num of republicans waiting in line for a booth
+int rep_inline = 0; 
+//num of democrats waiting in line for a booth
+int dem_inline = 0; 
+
+//Total number of voters given by argument
+int num_republicans = 0;
+int num_democrats = 0;
+int num_independents = 0;
+int num_booths = 0;
+
+int total_voting = 0; //total people voting today (set in main)
+int total_finished = 0; //total num people finished voting and left polling station
+int total_entered = 0; //toal num entered the polling station
+int *buffer = NULL; //Array that is printed to show booth status
+int total_waiting = 0; //count for the initial print waiting for station to open
+int in = 0;
+
+pthread_t *rep_threads;
+pthread_t *dem_threads;
+pthread_t *ind_threads;
+semaphore_t protect_count, mutex_lineup, barrier, printing_mutex, can_vote;
+semaphore_t free_booths;
+
+semaphore_t reps_wait_on_dems, dem_wait_on_reps;
+
+//protects the "in" count
+semaphore_t mutex_in;
+
+//for independents waiting to jump in line
+semaphore_t inds_waiting_for_line;
 
 /*****************************
  * Function Declarations
